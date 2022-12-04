f7866d7c668a9ea1efc524d4d9fe63860601a5ab
Jonathan Bastien-Filiatrault <joe@x2a.org>
2022-12-03 22:29:20 -0500

    Bidirectional bindings.

--- rustlf/build.rs
+++ rustlf/build.rs
@@ -1,25 +1,33 @@
-extern crate bindgen;
-extern crate cbindgen;
+use std::{env, error::Error, path::PathBuf};
 
-use std::env;
+fn main() -> Result<(), Box<dyn Error>> {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR")?;
 
-fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let glib = pkg_config::Config::new().probe("glib-2.0")?;
 
-    /*bindgen::Builder::default()
+    let includes = glib
+        .include_paths
+        .iter()
+        .map(|path| format!("-I{}", path.to_string_lossy()))
+        .chain(std::iter::once("-I..".to_owned()));
+
+    let bindings = bindgen::Builder::default()
+        .opaque_type("channel_cap")
         .header("../src/globalvars.h")
+        .clang_args(includes)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .clang_args(&[
-            "-I/usr/include/glib-2.0",
-            "-I/usr/lib/x86_64-linux-gnu/glib-2.0/include",
-        ]).generate()
-        .expect("Can't generate bindings");*/
+        .generate()
+        .map_err(|_| "could not generate bindings")?;
+
+    let out_path = PathBuf::from(env::var("OUT_DIR")?);
+    bindings.write_to_file(out_path.join("bindings.rs"))?;
 
     cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_language(cbindgen::Language::C)
         .with_include_guard("RUST_H")
-        .generate()
-        .expect("Unable to generate bindings")
+        .generate()?
         .write_to_file("rust.h");
+
+    Ok(())
 }
--- rustlf/src/lib.rs
+++ rustlf/src/lib.rs
@@ -1,1 +1,10 @@
+mod tlf {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(unused)]
+    #![allow(improper_ctypes)]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
 mod cw_utils;
