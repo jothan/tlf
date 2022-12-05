0d3e91f23094a95e69d8e70bf023195185236a1a
Jonathan Bastien-Filiatrault <joe@x2a.org>
2022-12-04 20:19:43 -0500

    Split off safe part.
    Having fun shaving the yak here.

--- rustlf/src/qtcutil.rs
+++ rustlf/src/qtcutil.rs
@@ -11,31 +11,36 @@ pub unsafe extern "C" fn parse_qtcline(
     callsign: *mut c_char,
     direction: c_uint,
 ) {
-    let logline = unsafe { CStr::from_ptr(logline) }.to_bytes();
+    let logline = CStr::from_ptr(logline).to_bytes();
+    let callsign = std::slice::from_raw_parts_mut(callsign as *mut u8, CALLSIGN_SIZE + 1);
 
-    let callsign =
-        unsafe { std::slice::from_raw_parts_mut(callsign as *mut u8, CALLSIGN_SIZE + 1) };
+    parse_qtcline_inner(logline, callsign, direction);
+}
 
+#[inline]
+fn parse_qtcline_inner(logline: &[u8], callsign: &mut [u8], direction: c_uint) {
     let offset = match direction {
         crate::tlf::RECV => QTC_RECV_OFFSET,
         crate::tlf::SEND => QTC_SEND_OFFSET,
         _ => unreachable!(),
     };
+
     let source = &logline[offset..offset + CALLSIGN_SIZE];
     let source_end = source
         .iter()
         .position(|c| *c == b' ')
         .unwrap_or(CALLSIGN_SIZE);
     let source_callsign = &source[..source_end];
+
     callsign[..source_callsign.len()].copy_from_slice(source_callsign);
     callsign[source_callsign.len()] = 0;
 }
 
 #[cfg(test)]
 mod tests {
-    use std::ffi::{c_char, CStr};
+    use std::ffi::CStr;
 
-    use super::{parse_qtcline, CALLSIGN_SIZE, QTC_RECV_OFFSET};
+    use super::{parse_qtcline_inner, CALLSIGN_SIZE, QTC_RECV_OFFSET};
     const QTC_LENGTH: usize = 120;
 
     fn make_line(callsign_length: usize, logline: &mut Vec<u8>) {
@@ -43,28 +48,26 @@ mod tests {
         logline.extend(std::iter::repeat(b' ').take(QTC_RECV_OFFSET));
         logline.extend(std::iter::repeat(b'A').take(callsign_length));
         logline.extend(std::iter::repeat(b' ').take(QTC_LENGTH - logline.len()));
-        *logline.last_mut().unwrap() = b'\n';
+        logline[QTC_LENGTH - 1] = b'\n';
     }
 
     #[test]
     fn test_parse_qtcline() {
         const TEST_SIZE: usize = 20;
-        let mut logline = Vec::with_capacity(TEST_SIZE);
-        let mut callsign = [-1; CALLSIGN_SIZE + 1];
+        let mut logline = Vec::with_capacity(QTC_LENGTH);
+        let mut callsign = [255; CALLSIGN_SIZE + 1];
 
-        for i in 0..20 {
+        for i in 0..TEST_SIZE {
             make_line(i, &mut logline);
-            callsign.fill(-1);
+            callsign.fill(255);
 
-            unsafe {
-                parse_qtcline(
-                    logline.as_ptr() as *const c_char,
-                    callsign.as_mut_ptr() as *mut c_char,
-                    crate::tlf::RECV,
-                )
-            };
+            parse_qtcline_inner(
+                logline.as_slice(),
+                callsign.as_mut_slice(),
+                crate::tlf::RECV,
+            );
 
-            let callsign = unsafe { CStr::from_ptr(callsign.as_ptr()) };
+            let callsign = unsafe { CStr::from_ptr(callsign.as_ptr() as *const i8) };
             assert_eq!(callsign.to_bytes().len(), std::cmp::min(i, CALLSIGN_SIZE));
         }
     }
