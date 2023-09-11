96d27826acfc8e242168e4fc94eb836f398ab173
Jonathan Bastien-Filiatrault <joe@x2a.org>
2023-09-11 12:24:57 -0400

    Missing crate stuff.

---
+++ rustlf/newtlf/build.rs
@@ -0,0 +1,16 @@
+use std::{env, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR")?;
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(cbindgen::Language::C)
+        .with_include_guard("NEWTLF_H")
+        .with_sys_include("curses.h")
+        .with_sys_include("hamlib/rig.h")
+        .generate()?
+        .write_to_file("newtlf.h");
+
+    Ok(())
+}
---
+++ src/newtlf.h
@@ -0,0 +1,1 @@
+../rustlf/newtlf/newtlf.h
